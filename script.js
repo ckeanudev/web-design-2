de78298b8b8124a3b2a75c998ed3bc1bcc868ed0
Ckeanu Richer Q. Locson <86586510+ckeanudev@users.noreply.github.com>
2021-07-19 16:55:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let container = document.querySelector(".container");
+
+
+function intro() {
+    container.style.transform = "translateX(0%)";
+} 
+function about() {
+    container.style.transform = "translateX(-100%)";
+} 
+function games() {
+    container.style.transform = "translateX(-200%)";
+} 
+function contact() {
+    container.style.transform = "translateX(-300%)";
+}
+
+
+
+
